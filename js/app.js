6f07b20cd6018c79c88fb88dca790617201071a8
flatrov <flatrov1@gmail.com>
2025-07-15 13:37:18 +0330

    add feture menu close btn

--- js/app.js
+++ js/app.js
@@ -1,26 +1,47 @@
 "use strict";
 const $ = document;
+const overlay = $.querySelector(".Overlay");
 const computerMenuWrapper = $.querySelector("#computer-menu-wrapper");
 const computerMenu = $.querySelector("#computer-menu");
-const navMenuComputer = $.querySelector("#nav-menu-computer");
 const mobileMenu = $.querySelector("#mobile-menu");
-const menuComputerBar = $.querySelector("#menu-computer-bar");
+const navMenuComputer = $.querySelector("#nav-menu-computer");
+const navMobileMenu = $.querySelector("#nav-mobile-menu");
+const menuComputerBtn = $.querySelector("#menu-computer-btn");
+const mobileMenuBtn = $.querySelector("#mobile-menu-btn");
+const mobileCloseBtn = $.querySelector("#mobile-close-btn");
+const overlayOpen = () => {
+    if (overlay instanceof HTMLElement) {
+        overlay.classList.remove("hidden");
+    }
+};
+const overlayClose = () => {
+    overlay?.classList.add("hidden");
+    const openElement = $.querySelector("[data-is-open=true]");
+    if (openElement instanceof HTMLElement) {
+        openElement.dataset.isOpen = "false";
+    }
+};
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             navMenuComputer.dataset.isOpen = "true";
             computerMenu.dataset.isFloat = "false";
-            mobileMenu.dataset.isMenu = "false";
+            if (navMobileMenu instanceof HTMLElement) {
+                navMobileMenu.dataset.isMenu = "false";
+            }
         }
         else {
             navMenuComputer.dataset.isOpen = "false";
             computerMenu.dataset.isFloat = "true";
-            mobileMenu.dataset.isMenu = "true";
+            if (navMobileMenu instanceof HTMLElement) {
+                navMobileMenu.dataset.isMenu = "true";
+            }
         }
     });
 });
 observer.observe(computerMenuWrapper);
-menuComputerBar.addEventListener("click", () => {
+overlay?.addEventListener("click", overlayClose);
+menuComputerBtn?.addEventListener("click", () => {
     if (navMenuComputer.dataset.isOpen == "true") {
         navMenuComputer.dataset.isOpen = "false";
     }
@@ -28,3 +49,10 @@ menuComputerBar.addEventListener("click", () => {
         navMenuComputer.dataset.isOpen = "true";
     }
 });
+mobileMenuBtn?.addEventListener("click", () => {
+    if (mobileMenu instanceof HTMLElement) {
+        mobileMenu.dataset.isOpen = "true";
+        overlayOpen();
+    }
+});
+mobileCloseBtn?.addEventListener("click", overlayClose);
