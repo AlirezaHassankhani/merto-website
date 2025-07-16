16942831f62f9e3c847479224dfa9e242ada937c
flatrov <flatrov1@gmail.com>
2025-07-16 12:08:31 +0330

    update js files

--- js/app.js
+++ js/app.js
@@ -9,6 +9,26 @@ const navMobileMenu = $.querySelector("#nav-mobile-menu");
 const menuComputerBtn = $.querySelector("#menu-computer-btn");
 const mobileMenuBtn = $.querySelector("#mobile-menu-btn");
 const mobileCloseBtn = $.querySelector("#mobile-close-btn");
+const loginBtn = $.querySelector(".login-btn");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            navMenuComputer.dataset.isSub = "true";
+            computerMenu.dataset.isFloat = "false";
+            if (navMobileMenu instanceof HTMLElement) {
+                navMobileMenu.dataset.isMenu = "false";
+            }
+        }
+        else {
+            navMenuComputer.dataset.isSub = "false";
+            computerMenu.dataset.isFloat = "true";
+            if (navMobileMenu instanceof HTMLElement) {
+                navMobileMenu.dataset.isMenu = "true";
+            }
+        }
+    });
+});
+observer.observe(computerMenuWrapper);
 const overlayOpen = () => {
     if (overlay instanceof HTMLElement) {
         overlay.classList.remove("hidden");
@@ -23,11 +43,11 @@ const overlayClose = () => {
 };
 overlay?.addEventListener("click", overlayClose);
 menuComputerBtn?.addEventListener("click", () => {
-    if (navMenuComputer.dataset.isOpen == "true") {
-        navMenuComputer.dataset.isOpen = "false";
+    if (navMenuComputer.dataset.isSub == "true") {
+        navMenuComputer.dataset.isSub = "false";
     }
     else {
-        navMenuComputer.dataset.isOpen = "true";
+        navMenuComputer.dataset.isSub = "true";
     }
 });
 mobileMenuBtn?.addEventListener("click", () => {
