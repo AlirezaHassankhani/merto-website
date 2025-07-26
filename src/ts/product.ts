6ad76cdcee6a94a33807b2bf79ee0b2616b602b4
flatrov <flatrov1@gmail.com>
2025-07-26 13:39:36 +0330

    change gallery logic

--- src/ts/product.ts
+++ src/ts/product.ts
@@ -1,24 +1,21 @@
-import Flatrov, { IGallery } from "./flatrov.js";
+import Flatrov, { IItem } from "./gallery.js";
 
-let items: IGallery[] = [
+let items: IItem[] = [
   {
     id: crypto.randomUUID(),
-    image: "../public/images/flatrov/cosmetics-40-5.jpg",
-    imageThumbnail: "../public/images/flatrov/cosmetics-40-2-300x300.jpg",
-    alt: "cosmetics",
+    src: "../public/images/flatrov/cosmetics-40-5.jpg",
+    thumbnail: "../public/images/flatrov/cosmetics-40-2-300x300.jpg",
   },
   {
     id: crypto.randomUUID(),
-    image: "../public/images/flatrov/cosmetics-40-3.jpg",
-    imageThumbnail: "../public/images/flatrov/cosmetics-40-3-300x300.jpg",
-    alt: "cosmetics",
+    src: "../public/images/flatrov/cosmetics-40-3.jpg",
+    thumbnail: "../public/images/flatrov/cosmetics-40-3-300x300.jpg",
   },
   {
     id: crypto.randomUUID(),
-    image: "../public/images/flatrov/cosmetics-40-4.jpg",
-    imageThumbnail: "../public/images/flatrov/cosmetics-40-4-300x300.jpg",
-    alt: "cosmetics",
+    src: "../public/images/flatrov/cosmetics-40-4.jpg",
+    thumbnail: "../public/images/flatrov/cosmetics-40-4-300x300.jpg",
   },
 ];
 
-let flatrov = new Flatrov(".flatrov", items);
+let flatrov = new Flatrov(".gallery", items);
